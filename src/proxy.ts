a76a8e4e35af7b3a6ad50bcc8752310a5acac88e
Lucky4all <lucasromerocoscia@gmail.com>
2025-11-01 15:34:06 -0300

    feat: implement proxy middleware for authentication handling

---
+++ src/proxy.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+
+export async function proxy(request: NextRequest) {
+    let cookie = request.cookies.get("token")
+    if (cookie) {
+        return NextResponse.next()
+    } else {
+        return NextResponse.redirect(new URL("/dashboard/login", request.url))
+    }
+}
+
+export const config = {
+    matcher: "/dashboard"
+}
